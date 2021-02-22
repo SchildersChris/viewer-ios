902620bff42bf8453d25f104263b9438531ddb11
Chris <chris-schilders@hotmail.nl>
2021-02-22 21:16:15 +0100

    Updated package and cleanup graphics view

--- Viewer/Views/ObjectDetailView.swift
+++ Viewer/Views/ObjectDetailView.swift
@@ -6,22 +6,37 @@
 //
 
 import SwiftUI
+import Rasterizer
 
 struct ObjectDetailView: View {
     let object: ObjectModel
 
-    @State private var zoom: Float = 1
-
+    @State private var translate: Vector3 = Vector3(x: 0, y: 0, z: -10)
+    @State private var rotate: Bool = true
+    
     var body: some View {
         VStack {
-            Text(object.name)
-            GraphicsViewController(300, 300, object, $zoom)
+            GraphicsViewController(
+                width: 300, height: 300,
+                object: object,
+                translate: $translate,
+                rotate: $rotate)
                 .frame(width: 300, height: 300)
                 .cornerRadius(10)
 
-            Slider(value: $zoom, in: 0...5, step: 0.1)
-            Text(String(format: "Current object distance level: %.1f", zoom))
-        }
+            Slider(value: $translate.x, in: -10...10, step: 0.1)
+            Text(String(format: "X translate value: %.1f", translate.x))
+            
+            Slider(value: $translate.y, in: -10...10, step: 0.1)
+            Text(String(format: "Y translate value: %.1f", translate.y))
+            
+            Slider(value: $translate.z, in: -20...20, step: 0.1)
+            Text(String(format: "Z translate value: %.1f", translate.z))
+            
+            Toggle(isOn: $rotate, label: {
+                Text("Rotate")
+            })
+        }.navigationBarTitle(Text(object.name), displayMode: .inline).padding()
         Spacer()
     }
 }
--- Viewer/Views/Shared/GraphicsViewController.swift
+++ Viewer/Views/Shared/GraphicsViewController.swift
@@ -10,19 +10,18 @@ struct GraphicsViewController : UIViewControllerRepresentable {
     let width: Int
     let height: Int
     let object: ObjectModel
-    var zoom: Binding<Float>
 
-    init(_ width: Int, _ height: Int, _ object: ObjectModel, _ zoom: Binding<Float>) {
-        self.width = width
-        self.height = height
-        self.object = object
-        self.zoom = zoom
-    }
-
+    var translate: Binding<Vector3>
+    var rotate: Binding<Bool>
+  
     func makeUIViewController(
             context: UIViewControllerRepresentableContext<GraphicsViewController>
     ) -> UIGraphicsViewController  {
-        UIGraphicsViewController(zoom, width, height, object.indices, object.vertices)
+        UIGraphicsViewController(
+            width, height,
+            object.indices,
+            object.vertices,
+            translate, rotate)
     }
 
     func updateUIViewController(
@@ -40,13 +39,15 @@ class UIGraphicsViewController : UIViewController, DisplayObserver {
     private let indices: [UInt32]
     private let vertices: [Vector3]
 
-    private var zBuffer: [Float]
-    private var rasterImage: CFMutableData
+    private var zBuffer: CFMutableData
+    private var frameBuffer: CFMutableData
 
     private var rotation: Float
-    private var zoom: Binding<Float>
+    
+    private var translate: Binding<Vector3>
+    private var rotate: Binding<Bool>
 
-    init(_ zoom: Binding<Float>, _ width: Int, _ height: Int, _ indices: [UInt32], _ vertices: [Vector3]) {
+    init(_ width: Int, _ height: Int, _ indices: [UInt32], _ vertices: [Vector3], _ translate: Binding<Vector3>, _ rotate: Binding<Bool>) {
         self.width = width
         self.height = height
         let count = width * height
@@ -54,14 +55,17 @@ class UIGraphicsViewController : UIViewController, DisplayObserver {
         self.indices = indices
         self.vertices = vertices
 
-        let size = MemoryLayout<UInt8>.size * count
-        rasterImage = CFDataCreateMutable(kCFAllocatorDefault, size)!
-        CFDataSetLength(rasterImage, size)
+        let frameSize = MemoryLayout<UInt8>.size * count
+        frameBuffer = CFDataCreateMutable(kCFAllocatorDefault, frameSize)!
+        CFDataSetLength(frameBuffer, frameSize)
 
-        zBuffer = Array<Float>(repeating: Float.greatestFiniteMagnitude, count: count)
+        let zBufferSize = MemoryLayout<Float>.size * count
+        zBuffer = CFDataCreateMutable(kCFAllocatorDefault, zBufferSize)!
+        CFDataSetLength(zBuffer, zBufferSize)
 
         rotation = 0
-        self.zoom = zoom
+        self.translate = translate
+        self.rotate = rotate
 
         super.init(nibName: nil, bundle: nil)
     }
@@ -80,7 +84,7 @@ class UIGraphicsViewController : UIViewController, DisplayObserver {
                 bytesPerRow: width,
                 space: CGColorSpaceCreateDeviceGray(),
                 bitmapInfo: CGBitmapInfo(rawValue: 0),
-                provider: CGDataProvider(data: rasterImage)!,
+                provider: CGDataProvider(data: frameBuffer)!,
                 decode: nil,
                 shouldInterpolate: true,
                 intent: CGColorRenderingIntent.defaultIntent)!))
@@ -95,20 +99,25 @@ class UIGraphicsViewController : UIViewController, DisplayObserver {
     }
 
     func update(_ deltaTime: CFTimeInterval) {
-        rotation += 1 * Float(deltaTime)
-
-        var matrix = Matrix4x4(
-                p1: Vector4(x: cosf(rotation), y: sinf(rotation), z: 0, w: 0),
-                p2: Vector4(x: -sinf(rotation), y: cosf(rotation), z: 0, w: 0),
-                p3: Vector4(x: 0, y: 0, z: zoom.wrappedValue, w: 0),
-                p4: Vector4(x: 0, y: 0, z: 0, w: 1))
-
+        if rotate.wrappedValue {
+            rotation += 1 * Float(deltaTime)
+        }
+        
+        let t = translate.wrappedValue
+        var mat = Matrix4x4(
+            p1: Vector4(x: cosf(rotation), y:0, z: sinf(rotation), w: 0),
+            p2: Vector4(x: 0, y: 1, z: 0, w: 0),
+            p3: Vector4(x: -sinf(rotation), y: 0, z: cosf(rotation), w: 0),
+            p4: Vector4(x: t.x, y: t.y, z: t.z, w: 1))
+        
         rasterize(
             vertices, indices,
             UInt32(indices.count),
-            &matrix, &zBuffer,
-            UInt32(width), UInt32(height),
-            CFDataGetMutableBytePtr(rasterImage))
+            &mat,
+            CFDataGetMutableBytePtr(zBuffer)
+                .withMemoryRebound(to: Float.self, capacity: width * height) { ptr in ptr },
+            CFDataGetMutableBytePtr(frameBuffer),
+            UInt32(width), UInt32(height))
 
         imageView?.image = UIImage(cgImage: CGImage(
             width: width,
@@ -118,7 +127,7 @@ class UIGraphicsViewController : UIViewController, DisplayObserver {
             bytesPerRow: width,
             space: CGColorSpaceCreateDeviceGray(),
             bitmapInfo: CGBitmapInfo(rawValue: 0),
-            provider: CGDataProvider(data: rasterImage)!,
+            provider: CGDataProvider(data: frameBuffer)!,
             decode: nil,
             shouldInterpolate: true,
             intent: CGColorRenderingIntent.defaultIntent)!)
